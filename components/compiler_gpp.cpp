3ca6d7927dbeedeafea21c7505f0ce85911bda4d
xiaodaolin <fideojlin@gmail.com>
2019-04-03 14:16:44 +0800

    完成compiler_gpp、tester

--- components/compiler_gpp.cpp
+++ components/compiler_gpp.cpp
@@ -1,24 +1,266 @@
+#include "compiler_gpp.pb.h"
+#include "file_agent.hpp"
 #include "mdwrkapi.hpp"
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+extern "C" {
+#include "sandbox.h"
+}
+
+using std::string;
+
+DEFINE_string(workspace, "./workspace", "workspace");
+DEFINE_string(file_provider, "", "local file provider");
+DEFINE_string(compile_script, "./compile.sh", "compile script location");
+DEFINE_string(broker_addr, "tcp://broker:5555", "broker address");
+DEFINE_string(service_name, "compiler_gpp", "service name");
+DEFINE_bool(verbose, false, "verbose");
+
+class CompilerGpp {
+private:
+  enum class _PathType { SUBMISSION = 0, PROBLEM };
+
+public:
+  CompilerGpp(const string &workspace, const string &file_provider,
+              const string &compile_script, const string &broker_addr,
+              const string &service_name = "compiler_gpp", bool verbose = false)
+      : session_(broker_addr, service_name, verbose), verbose_(verbose),
+        broker_addr_(broker_addr), workspace_(workspace),
+        file_provider_(file_provider), compile_script_(compile_script) {
+    if (access(compile_script.c_str(), X_OK) < 0)
+      LOG_FATAL_SYS_ERR;
+    if (mkdir(workspace_.c_str(), 0755) < 0 && errno != EEXIST)
+      LOG_FATAL_SYS_ERR;
+    string sub_dir = workspace_ + "/submission";
+    if (mkdir(sub_dir.c_str(), 0755) < 0 && errno != EEXIST)
+      LOG_FATAL_SYS_ERR;
+    string prob_dir = workspace_ + "/problem";
+    if (mkdir(prob_dir.c_str(), 0755) < 0 && errno != EEXIST)
+      LOG_FATAL_SYS_ERR;
+  }
+
+  void run() {
+    zmsg *z_rsp = nullptr;
+    while (1) {
+      zmsg *z_req = session_.recv(z_rsp);
+      //  interrupted
+      if (!z_req) {
+        break;
+      }
+      handle(z_req, z_rsp);
+    }
+  }
+
+private:
+  void handle(zmsg *&z_req, zmsg *&z_rsp) {
+    compiler_gpp::Request req;
+    req.ParseFromArray(z_req->body(), z_req->body_size());
+    compiler_gpp::Response rsp;
+    handle_compiling(req, rsp);
+    rsp.set_request_id(req.request_id());
+    rsp.set_file_provider(file_provider_);
+    string rsp_str = rsp.SerializeAsString();
+    delete z_req;
+    z_rsp = new zmsg(rsp_str.c_str(), rsp_str.size());
+  }
+
+  void handle_compiling(const compiler_gpp::Request &req,
+                        compiler_gpp::Response &rsp) {
+    // get src and header files
+    FileAgent agent(workspace_, broker_addr_, req.file_provider(), verbose_);
+    for (int idx = 0; idx < req.sub_src_filename_size(); ++idx) {
+      if (!ensure_file(_PathType::SUBMISSION, req.sub_src_filename(idx), req,
+                       &agent)) {
+        rsp.set_result(compiler_gpp::Response::SYSTEM_ERROR);
+        return;
+      }
+    }
+    for (int idx = 0; idx < req.sub_header_filename_size(); ++idx) {
+      if (!ensure_file(_PathType::SUBMISSION, req.sub_header_filename(idx), req,
+                       &agent)) {
+        rsp.set_result(compiler_gpp::Response::SYSTEM_ERROR);
+        return;
+      }
+    }
+    for (int idx = 0; idx < req.prob_src_filename_size(); ++idx) {
+      if (!ensure_file(_PathType::PROBLEM, req.prob_src_filename(idx), req,
+                       &agent)) {
+        rsp.set_result(compiler_gpp::Response::SYSTEM_ERROR);
+        return;
+      }
+    }
+    for (int idx = 0; idx < req.prob_header_filename_size(); ++idx) {
+      if (!ensure_file(_PathType::PROBLEM, req.prob_header_filename(idx), req,
+                       &agent)) {
+        rsp.set_result(compiler_gpp::Response::SYSTEM_ERROR);
+        return;
+      }
+    }
+
+    struct result ret = {};
+    do_compiling(&ret, req);
+    set_compiling_result(&ret, req, rsp);
+  }
+
+  string path_of(const string &workspace, _PathType path_type, int id,
+                 const string &filename) {
+    string path;
+    if (!workspace.empty()) {
+      path += workspace;
+      path += '/';
+    }
+    switch (path_type) {
+    case _PathType::SUBMISSION:
+      path += "submission/";
+      break;
+    case _PathType::PROBLEM:
+      path += "problem/";
+      break;
+    default:
+      return "";
+    }
+    path += std::to_string(id);
+    path += '/';
+    path += filename;
+    return path;
+  }
+
+  bool ensure_file(_PathType path_type, const string &filename,
+                   const compiler_gpp::Request &req, FileAgent *agent) {
+    int id = path_type == _PathType::SUBMISSION ? req.sub_id() : req.prob_id();
+    string path = path_of("", path_type, id, filename);
+    if (req.file_provider() != file_provider_)
+      return agent && agent->fetch(path);
+
+    string full_path = workspace_ + '/' + path;
+    if (access(full_path.c_str(), R_OK) < 0) {
+      if (errno == ENOTDIR || errno == ENOENT) {
+        return agent && agent->fetch(path);
+      } else {
+        LOG_SYS_ERR;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void do_compiling(struct result *ret, const compiler_gpp::Request &req) {
+    struct config cfg;
+    cfg.max_cpu_time = 10 * 1000;
+    cfg.max_real_time = 15 * 1000;
+    cfg.memory_limit_check_only = 0;
+    cfg.max_memory = 128 * 1024 * 1024;
+    cfg.max_process_number = 200;
+    cfg.max_output_size = 20 * 1024 * 1024;
+    cfg.max_stack = 32 * 1024 * 1024;
+    cfg.seccomp_rule_name = nullptr;
+    cfg.uid = -1;
+    cfg.gid = -1;
+
+    cfg.exe_path = (char *)compile_script_.c_str();
+    cfg.input_path = nullptr;
+
+    string output_path =
+        path_of(workspace_, _PathType::SUBMISSION, req.sub_id(), "compile.log");
+    cfg.output_path = (char *)output_path.c_str();
+    cfg.error_path = cfg.output_path;
+
+    string log_path =
+        path_of(workspace_, _PathType::SUBMISSION, req.sub_id(), "sandbox.log");
+    cfg.log_path = (char *)log_path.c_str();
+
+    cfg.env[0] = nullptr;
+    cfg.args[0] = (char *)"compile.sh";
+
+    string include_dir1 =
+        path_of(workspace_, _PathType::SUBMISSION, req.sub_id(), "");
+    string include_dir2 =
+        path_of(workspace_, _PathType::PROBLEM, req.prob_id(), "");
+    cfg.args[1] = (char *)include_dir1.c_str();
+    cfg.args[2] = (char *)include_dir2.c_str();
+    cfg.args[3] = (char *)include_dir1.c_str();
+
+    int argc = 4;
+    std::vector<string> src_path_vec;
+    for (int idx = 0; idx < req.sub_src_filename_size(); ++idx) {
+      src_path_vec.emplace_back(path_of(workspace_, _PathType::SUBMISSION,
+                                        req.sub_id(),
+                                        req.sub_src_filename(idx)));
+      cfg.args[argc + idx] = (char *)src_path_vec[idx].c_str();
+    }
+    argc += req.sub_src_filename_size();
+    for (int idx = 0; idx < req.prob_src_filename_size(); ++idx) {
+      src_path_vec.emplace_back(path_of(workspace_, _PathType::PROBLEM,
+                                        req.prob_id(),
+                                        req.prob_src_filename(idx)));
+      cfg.args[argc + idx] = (char *)src_path_vec[idx].c_str();
+    }
+    argc += req.prob_src_filename_size();
+    cfg.args[argc] = nullptr;
+
+    ::run(&cfg, ret);
+    VLOG(1) << ret->cpu_time << " " << ret->real_time << " "
+      << ret->memory << " " << ret->signal << " "
+      << ret->exit_code << " " << ret->error << " "
+      << ret->result;
+  }
+
+  void set_compiling_result(const struct result *ret,
+                            const compiler_gpp::Request &req,
+                            compiler_gpp::Response &rsp) {
+    rsp.set_result(compiler_gpp::Response::SYSTEM_ERROR);
+    if (ret->result == ::SYSTEM_ERROR)
+      return;
+    if (ret->result == ::RUNTIME_ERROR && ret->exit_code == 1) {
+      string compile_log = path_of(workspace_, _PathType::SUBMISSION,
+                                   req.sub_id(), "compile.log");
+      FILE *file = fopen(compile_log.c_str(), "r");
+      if (!file) {
+        LOG_SYS_ERR;
+        return;
+      }
+      size_t nread = fread(log_buf_, 1, max_log_size, file);
+      if (ferror(file)) {
+        LOG_SYS_ERR;
+        return;
+      }
+      if (!feof(file)) {
+        strncpy(log_buf_ + max_log_size - 4, "...", 4);
+      } else {
+        log_buf_[nread] = '\0';
+      }
+      rsp.set_result(compiler_gpp::Response::FAIL);
+      rsp.set_detail(log_buf_);
+    } else if (ret->result == ::SUCCESS) {
+      rsp.set_result(compiler_gpp::Response::SUCCESS);
+    } else {
+      rsp.set_result(compiler_gpp::Response::FAIL);
+      rsp.set_detail("Malicious code detected.");
+    }
+  }
+
+private:
+  mdwrk session_;
+  bool verbose_;
+  string broker_addr_, workspace_, file_provider_, compile_script_;
+  static constexpr size_t max_log_size = 10 * 1024;
+  char log_buf_[max_log_size];
+};
 
 int main(int argc, char *argv[]) {
-  mdwrk session("tcp://broker:5555", "Compiler:g++", 1);
-
-  zmsg *reply = 0;
-  char sys_comand[101];
-  while (1) {
-    zmsg *request = session.recv(reply);
-    if (request == 0) {
-      break; //  Worker was interrupted
-    }
-    snprintf(sys_comand, 100, "g++ %s -std=c++11 -o submission",
-             request->body());
-    int ret = system(sys_comand);
-    if (!ret) {
-      reply = new zmsg("Compilation passed.");
-    } else {
-      reply = new zmsg("Compilation failed.");
-    }
-    delete request;
-  }
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+  if (FLAGS_file_provider.empty())
+    LOG(FATAL) << "local file provider not set.";
+  CompilerGpp compiler(FLAGS_workspace, FLAGS_file_provider,
+                       FLAGS_compile_script, FLAGS_broker_addr,
+                       FLAGS_service_name, FLAGS_verbose);
+  compiler.run();
   return 0;
 }
--- components/tester.cpp
+++ components/tester.cpp
@@ -1,62 +1,343 @@
+#include "file_agent.hpp"
+#include "logger.hpp"
 #include "mdwrkapi.hpp"
-#include <iostream>
+#include "tester.pb.h"
+#include "thread_pool.hpp"
+#include <ctype.h>
+#include <functional>
+#include <future>
+#include <gflags/gflags.h>
+#include <stdio.h>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <thread>
+#include <unistd.h>
+#include <vector>
 extern "C" {
 #include "sandbox.h"
 }
 
-class tester {
+using std::string;
+
+DEFINE_string(workspace, "./workspace", "workspace");
+DEFINE_string(file_provider, "", "local file provider");
+DEFINE_string(broker_addr, "tcp://broker:5555", "broker address");
+DEFINE_string(service_name, "tester", "service name");
+DEFINE_bool(verbose, false, "verbose");
+DEFINE_uint64(thread_pool_size, 0, "thread pool size");
+
+class Tester {
+private:
+  enum class _PathType { SUBMISSION = 0, PROBLEM };
+
 public:
+  Tester(const string &workspace, const string &file_provider,
+         const string &broker_addr, const string &service_name = "tester",
+         bool verbose = false, size_t thread_pool_size = 8)
+      : session_(broker_addr, service_name, verbose), verbose_(verbose),
+        broker_addr_(broker_addr), workspace_(workspace),
+        file_provider_(file_provider), thread_pool_(thread_pool_size) {
+    if (mkdir(workspace_.c_str(), 0755) < 0 && errno != EEXIST)
+      LOG_FATAL_SYS_ERR;
+    string sub_dir = workspace_ + "/submission";
+    if (mkdir(sub_dir.c_str(), 0755) < 0 && errno != EEXIST)
+      LOG_FATAL_SYS_ERR;
+    string prob_dir = workspace_ + "/problem";
+    if (mkdir(prob_dir.c_str(), 0755) < 0 && errno != EEXIST)
+      LOG_FATAL_SYS_ERR;
+  }
+
   void run() {
-    mdwrk session("tcp://broker:5555", "tester", 1);
-    zmsg *rsp = nullptr;
+    zmsg *z_rsp = nullptr;
     while (1) {
-      zmsg *req = session.recv(rsp);
-      if (!req) {
-        break; //  Worker was interrupted
+      zmsg *z_req = session_.recv(z_rsp);
+      //  interrupted
+      if (!z_req) {
+        break;
       }
-      handle(req, rsp);
+      handle(z_req, z_rsp);
     }
   }
 
 private:
-  void handle(zmsg *&req, zmsg *&rsp) {
-    struct result ret = test();
-    std::cout << ret.result << " " << ret.error << std::endl;
-    if (ret.result == SUCCESS && ret.error == SUCCESS) {
-      rsp = new zmsg("tests passed.");
-    } else {
-      rsp = new zmsg("tests failed.");
-    }
-    delete req;
-    req = nullptr;
+  void handle(zmsg *&z_req, zmsg *&z_rsp) {
+    tester::Request req;
+    req.ParseFromArray(z_req->body(), z_req->body_size());
+    tester::Response rsp;
+    handle_testing(req, rsp);
+    rsp.set_request_id(req.request_id());
+    rsp.set_file_provider(file_provider_);
+    string rsp_str = rsp.SerializeAsString();
+    delete z_req;
+    z_rsp = new zmsg(rsp_str.c_str(), rsp_str.size());
   }
 
-  struct result test() {
+  void handle_testing(const tester::Request &req, tester::Response &rsp) {
+    // get exectable file
+    FileAgent agent(workspace_, broker_addr_, req.exec_file_provider(),
+                    verbose_);
+    if (!ensure_file(_PathType::SUBMISSION, "submission", req,
+                     req.exec_file_provider(), &agent)) {
+      rsp.set_result(tester::Response::SYSTEM_ERROR);
+      return;
+    };
+    string exec_file =
+        path_of(workspace_, _PathType::SUBMISSION, req.sub_id(), "submission");
+    if (chmod(exec_file.c_str(), 0755) < 0) {
+      LOG_SYS_ERR;
+      rsp.set_result(tester::Response::SYSTEM_ERROR);
+      return;
+    }
+    // run each test case in thread pool
+    int test_case_id_size = req.test_case_id_size();
+    std::vector<std::future<void>> future_vec;
+    std::vector<struct result> ret_vec(test_case_id_size);
+    for (int idx = 0; idx < req.test_case_id_size(); ++idx) {
+      future_vec.emplace_back(thread_pool_.enqueue(std::bind(
+          &Tester::do_testing, this, &ret_vec[idx], std::cref(req), idx)));
+    }
+    // read results and set rsp
+    // init rsp result as the best one
+    rsp.set_result(tester::Response::ACCEPTED);
+    for (int idx = 0; idx < req.test_case_id_size(); ++idx) {
+      future_vec[idx].get();
+      struct result *ret = &ret_vec[idx];
+      auto test_case = rsp.add_test_case();
+      test_case->set_test_case_id(req.test_case_id(idx));
+      test_case->set_cpu_time(ret->cpu_time);
+      test_case->set_memory(ret->memory);
+      test_case->set_exit_code(ret->exit_code);
+      test_case->set_signal(ret->signal);
+      switch (ret->result) {
+      case ::CPU_TIME_LIMIT_EXCEEDED:
+      case ::REAL_TIME_LIMIT_EXCEEDED:
+        test_case->set_result(tester::Response::TIME_LIMIT_EXCEEDED);
+        break;
+      case ::MEMORY_LIMIT_EXCEEDED:
+        test_case->set_result(tester::Response::MEMORY_LIMIT_EXCEEDED);
+        break;
+      case ::RUNTIME_ERROR:
+        test_case->set_result(tester::Response::RUNTIME_ERROR);
+        break;
+      case ::SYSTEM_ERROR:
+        test_case->set_result(tester::Response::SYSTEM_ERROR);
+        break;
+      default:
+        // exit normally
+        string actural_out_file =
+            path_of(workspace_, _PathType::SUBMISSION, req.sub_id(),
+                    std::to_string(req.test_case_id(idx)) + ".actural");
+        string expected_out_file =
+            path_of(workspace_, _PathType::PROBLEM, req.prob_id(),
+                    std::to_string(req.test_case_id(idx)) + ".out");
+        test_case->set_result(compare(actural_out_file, expected_out_file));
+      }
+      // set rsp result as the WORST result of test cases
+      if (test_case->result() > rsp.result())
+        rsp.set_result(test_case->result());
+    }
+  }
+
+  string path_of(const string &workspace, _PathType path_type, int id,
+                 const string &filename) {
+    string path;
+    if (!workspace.empty()) {
+      path += workspace;
+      path += '/';
+    }
+    switch (path_type) {
+    case _PathType::SUBMISSION:
+      path += "submission/";
+      break;
+    case _PathType::PROBLEM:
+      path += "problem/";
+      break;
+    default:
+      return "";
+    }
+    path += std::to_string(id);
+    path += '/';
+    path += filename;
+    return path;
+  }
+
+  bool ensure_file(_PathType path_type, const string &filename,
+                   const tester::Request &req, const string &file_provider,
+                   FileAgent *agent) {
+    int id = path_type == _PathType::SUBMISSION ? req.sub_id() : req.prob_id();
+    string path = path_of("", path_type, id, filename);
+    if (file_provider != file_provider_)
+      return agent && agent->fetch(path);
+
+    string full_path = workspace_ + '/' + path;
+    if (access(full_path.c_str(), R_OK) < 0) {
+      if (errno == ENOTDIR || errno == ENOENT) {
+        return agent && agent->fetch(path);
+      } else {
+        LOG_SYS_ERR;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void do_testing(struct result *ret, const tester::Request &req, int idx) {
+    string in_filename = std::to_string(req.test_case_id(idx)) + ".in";
+    string expected_out_filename =
+        std::to_string(req.test_case_id(idx)) + ".out";
+    FileAgent agent(workspace_, broker_addr_, req.test_file_provider(),
+                    verbose_);
+    if (!(ensure_file(_PathType::PROBLEM, in_filename, req,
+                      req.test_file_provider(), &agent) &&
+          ensure_file(_PathType::PROBLEM, expected_out_filename, req,
+                      req.test_file_provider(), &agent))) {
+      ret->result = ::SYSTEM_ERROR;
+      return;
+    }
+    run_test_case(ret, req, idx);
+  }
+
+  void run_test_case(struct result *ret, const tester::Request &req, int idx) {
     struct config cfg;
-    cfg.max_cpu_time = 1000;
-    cfg.max_real_time = 5000;
-    cfg.max_memory = 128 * 1024 * 1024;
+    cfg.max_cpu_time = req.max_cpu_time();
+    cfg.max_real_time = req.max_cpu_time() * 2;
+    cfg.memory_limit_check_only = 0;
+    cfg.max_memory = req.max_memory();
     cfg.max_process_number = 200;
-    cfg.max_output_size = 10000;
+    cfg.max_output_size = 1024 * 1024;
     cfg.max_stack = 32 * 1024 * 1024;
-    cfg.exe_path = "submission";
-    cfg.input_path = "1.in";
-    cfg.output_path = "1.out";
-    cfg.error_path = "1.out";
-    cfg.args[0] = NULL;
-    cfg.env[0] = NULL;
-    cfg.log_path = "judger.log";
-    cfg.seccomp_rule_name = "c_cpp";
+    string exec_file =
+        path_of(workspace_, _PathType::SUBMISSION, req.sub_id(), "submission");
+    string in_file = path_of(workspace_, _PathType::PROBLEM, req.prob_id(),
+                             std::to_string(req.test_case_id(idx)) + ".in");
+    string actural_out_file =
+        path_of(workspace_, _PathType::SUBMISSION, req.sub_id(),
+                std::to_string(req.test_case_id(idx)) + ".actural");
+    cfg.exe_path = (char *)exec_file.c_str();
+    cfg.input_path = (char *)in_file.c_str();
+    cfg.output_path = (char *)actural_out_file.c_str();
+    cfg.error_path = (char *)actural_out_file.c_str();
+    cfg.args[0] = nullptr;
+    cfg.env[0] = nullptr;
+    string log_path =
+        path_of(workspace_, _PathType::SUBMISSION, req.sub_id(), "sandbox.log");
+    cfg.log_path = (char *)log_path.c_str();
+    cfg.seccomp_rule_name = (char *)"c_cpp";
+    // nobody user
     cfg.uid = 65534;
     cfg.gid = 65534;
-    struct result ret = {};
-    ::run(&cfg, &ret);
-    return ret;
+    ::run(&cfg, ret);
   }
+
+  tester::Response::ResultType compare(const string &actural,
+                                       const string &expected) {
+    _CharStream s1(actural), s2(expected);
+    char ch1, ch2;
+    bool ret1, ret2;
+    while (1) {
+      ret1 = s1.next_char(ch1);
+      ret2 = s2.next_char(ch2);
+      if (s1.error() || s2.error())
+        return tester::Response::SYSTEM_ERROR;
+      if (!ret1 && !ret2)
+        return tester::Response::ACCEPTED;
+      if (!ret1 || !ret2 || ch1 != ch2)
+        break;
+    }
+    while (1) {
+      ret1 = s1.next_non_space_char(ch1);
+      ret2 = s2.next_non_space_char(ch2);
+      if (s1.error() || s2.error())
+        return tester::Response::SYSTEM_ERROR;
+      if (!ret1 && !ret2)
+        return tester::Response::PRESENTATION_ERROR;
+      if (!ret1 || !ret2 || ch1 != ch2)
+        return tester::Response::WRONG_ANSWER;
+    }
+  }
+
+private:
+  class _CharStream {
+  public:
+    _CharStream(const string &path) {
+      file_ = fopen(path.c_str(), "r");
+      if (!file_) {
+        LOG_SYS_ERR;
+        error_ = true;
+      } else {
+        nread_ = fread(buf_, 1, chunk_size_, file_);
+        if (ferror(file_)) {
+          LOG_SYS_ERR;
+          error_ = true;
+        }
+      }
+    }
+
+    ~_CharStream() {
+      if (file_ && fclose(file_) == EOF)
+        LOG_SYS_ERR;
+    }
+
+    bool error() const { return error_; }
+
+    bool next_char(char &ch) {
+      if (error_)
+        return false;
+      if (idx_ == nread_) {
+        if (feof(file_))
+          return false;
+        nread_ = fread(buf_, 1, chunk_size_, file_);
+        if (ferror(file_)) {
+          LOG_SYS_ERR;
+          error_ = true;
+          return false;
+        }
+        if (nread_ == 0)
+          return false;
+        idx_ = 0;
+      }
+      ch = buf_[idx_];
+      ++idx_;
+      return true;
+    }
+
+    bool next_non_space_char(char &ch) {
+      bool has_next_char;
+      while ((has_next_char = next_char(ch)) && isspace(ch))
+        ;
+      return has_next_char;
+    }
+
+  private:
+    FILE *file_;
+    static constexpr size_t chunk_size_ = 4096;
+    char buf_[chunk_size_];
+    size_t nread_;
+    size_t idx_ = 0;
+    bool error_ = false;
+  };
+
+private:
+  mdwrk session_;
+  bool verbose_;
+  string broker_addr_, workspace_, file_provider_;
+  ThreadPool thread_pool_;
 };
 
-int main() {
-  tester tester;
+int main(int argc, char *argv[]) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+  if (FLAGS_file_provider.empty())
+    LOG(FATAL) << "local file provider not set.";
+  size_t thread_pool_size = FLAGS_thread_pool_size > 0
+                                ? FLAGS_thread_pool_size
+                                : (std::thread::hardware_concurrency() > 0
+                                       ? std::thread::hardware_concurrency()
+                                       : 2);
+  Tester tester(FLAGS_workspace, FLAGS_file_provider, FLAGS_broker_addr,
+                FLAGS_service_name, FLAGS_verbose, thread_pool_size);
   tester.run();
   return 0;
 }
